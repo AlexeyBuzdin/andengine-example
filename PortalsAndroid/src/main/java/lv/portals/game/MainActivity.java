a186802ad2b46c5cbfa7babf29899134c4525205
LArchaon <alex.buzdin@gmail.com>
2013-06-28 00:21:47 +0300

    AndEngine added as dependency

--- PortalsAndroid/src/main/java/lv/portals/game/MainActivity.java
+++ PortalsAndroid/src/main/java/lv/portals/game/MainActivity.java
@@ -4,20 +4,61 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.engine.options.EngineOptions;
+import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.entity.scene.background.ColorBackground;
+import org.anddev.andengine.entity.util.FPSLogger;
+import org.anddev.andengine.ui.activity.BaseGameActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends BaseGameActivity {
+
+// ===========================================================
+// Constants
+// ===========================================================
+
+    private static final int CAMERA_WIDTH = 720;
+    private static final int CAMERA_HEIGHT = 480;
+
+// ===========================================================
+// Fields
+// ===========================================================
+
+    private Camera mCamera;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        TextView child = new TextView(this);
-        child.setText("Hello World from the App");
-
-        LinearLayout layout = new LinearLayout(this);
-        layout.addView(child);
-
-        setContentView(layout);
+    public Engine onLoadEngine() {
+        this.mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+        return new Engine(new EngineOptions(true, EngineOptions.ScreenOrientation.LANDSCAPE, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), this.mCamera));
     }
 
+    @Override
+    public void onLoadResources() {
+
+    }
+
+    @Override
+    public Scene onLoadScene() {
+        this.mEngine.registerUpdateHandler(new FPSLogger());
+
+        final Scene scene = new Scene(1);
+        scene.setBackground(new ColorBackground(0.09804f, 0.6274f, 0.8784f));
+
+        return scene;
+    }
+
+    @Override
+    public void onLoadComplete() {
+
+    }
+
+// ===========================================================
+// Methods
+// ===========================================================
+
+// ===========================================================
+// Inner and Anonymous Classes
+// ===========================================================
 }
