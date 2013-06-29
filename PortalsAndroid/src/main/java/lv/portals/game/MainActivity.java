2adc6eec1dd8b0d35f5b03d29caedcdbecb3f0d2
LArchaon <alex.buzdin@gmail.com>
2013-06-29 18:30:56 +0300

    Cleared MainActivity

--- PortalsAndroid/src/main/java/lv/portals/game/MainActivity.java
+++ PortalsAndroid/src/main/java/lv/portals/game/MainActivity.java
@@ -3,21 +3,27 @@ package lv.portals.game;
 import org.anddev.andengine.engine.Engine;
 import org.anddev.andengine.engine.camera.Camera;
 import org.anddev.andengine.engine.options.EngineOptions;
+import org.anddev.andengine.engine.options.WakeLockOptions;
 import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.anddev.andengine.entity.scene.Scene;
 import org.anddev.andengine.entity.scene.background.ColorBackground;
-import org.anddev.andengine.entity.util.FPSLogger;
 import org.anddev.andengine.ui.activity.BaseGameActivity;
 
 public class MainActivity extends BaseGameActivity {
 
-    private static final int CAMERA_WIDTH = 720;
-    private static final int CAMERA_HEIGHT = 480;
+    private static final int WIDTH = 720;
+    private static final int HEIGHT = 480;
+
+    private Scene scene;
 
     @Override
     public Engine onLoadEngine() {
-        Camera mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
-        return new Engine(new EngineOptions(true, EngineOptions.ScreenOrientation.LANDSCAPE, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), mCamera));
+        Camera camera = new Camera(0, 0, WIDTH, HEIGHT);
+
+        EngineOptions engineOptions = new EngineOptions(true, EngineOptions.ScreenOrientation.LANDSCAPE, new RatioResolutionPolicy(WIDTH, HEIGHT), camera);
+        engineOptions.setWakeLockOptions(WakeLockOptions.SCREEN_ON);
+
+        return new Engine(engineOptions);
     }
 
     @Override
@@ -27,10 +33,10 @@ public class MainActivity extends BaseGameActivity {
 
     @Override
     public Scene onLoadScene() {
-        this.mEngine.registerUpdateHandler(new FPSLogger());
+//        this.mEngine.registerUpdateHandler(new FPSLogger());
 
-        final Scene scene = new Scene(1);
-        scene.setBackground(new ColorBackground(0.09804f, 0.6274f, 0.8784f));
+        scene = new Scene();
+        scene.setBackground(new ColorBackground(0.05804f, 0.1274f, 0.3784f));
 
         return scene;
     }
